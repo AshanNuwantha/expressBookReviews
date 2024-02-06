e98a4020ac149661b7603c630dcd0661b47ccf0f
AshanNuwantha <AshanNuwantha@outlook.com>
2024-02-06 16:30:40 +0530

    FinalProject Node.js IBM course "Ashan"

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,26 +3,66 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+    {
+      "username":"Nimal",
+      "password":"123book"
+    },
+    {
+      "username":"Amal",
+      "password":"1234xyz"
+    }
+];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  var isValid = false;
+  users.forEach(function(user){
+    if(user.username == username && user.username !== undefined && user.username != 0){
+      isValid = true;
+    }
+  });
+  return isValid;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  var isExists = false;
+  users.forEach(function(user){
+    if(user.username !== undefined && user.username != 0 && user.password !== undefined && user.password != 0 && user.username == username && user.password == password){
+      isExists = true;
+    }
+  });
+  return isExists;
 }
 
-//only registered users can login
+//only registered users can login (Task 7)
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const{username,password} = req.body;
+  if(authenticatedUser(username,password)){
+    let accessToken = jwt.sign({data: username}, 'access', { expiresIn: 60 * 60 });
+    req.session.authenticated ={accessToken}
+    return res.status(403).json({message: "User successfully logged in.", accessToken: accessToken});
+  }else{
+    return res.status(403).json({message: "username & password not vaild."});
+  }
 });
 
-// Add a book review
+// Add a book review (Task 8)
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const review = req.body.reviews;
+  if( review === undefined || review == 0){ return res.status(300).json({message :"undefined reviews"});}
+  else{ books[req.params.isbn].reviews = review;  return res.status(300).json({message: "success book review added or modify.","review": books[req.params.isbn].reviews}); }
+});
+
+// Delete a book review under isbn (Task 9)
+regd_users.delete("/auth/review/:isbn", function(req, res){
+  const isbn = req.params.isbn;
+  if(isbn === undefined || isbn == 0){
+    return res.status(403).json({message :"undefined isbn"});}
+  else{ 
+    books[req.params.isbn].reviews = {}; 
+    var deletedBookReviewDetail = books[req.params.isbn];
+    return res.status(300).json({message: "success book review deleted.", deletedBookReviewDetail}); 
+  }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,137 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+// Register User (Task 6)
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const{username,password} = req.body;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Body Empty"});
+  }
+  if(isValid(username)){
+    return res.status(404).json({message: "User already exists"});
+  }else{
+    index = users.push({username:username,password:password});
+    return res.status(300).json({message: "Success registation.",registation_index: index });
+  }
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop (Task 1)
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if(books === undefined || books.length == 0){
+    return res.status(404).json({message: "No more results."});
+  }else{
+    return res.status(300).json({books});//.send(JSON.stringify({books},null,4))
+  }
 });
 
-// Get book details based on ISBN
+// Get book details based on ISBN (Task 2)
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  if(books[isbn] === undefined || books[isbn].length == 0){
+    return res.status(404).json({message: "No more results."});
+  }else{
+    return res.status(300).json(books[req.params.isbn]);//.send(JSON.stringify({books},null,4))
+  }
+});
   
-// Get book details based on author
+// Get book details based on author (Task 3)
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  var resultSet = [];
+  for(var keys in books){ if(books[keys].author == req.params.author){ resultSet.push(books[keys])} }
+
+  if(resultSet === undefined || resultSet.length == 0){
+    return res.status(404).json({message: "No more results."});
+  }else{
+    return res.status(300).json(resultSet);
+  }
+
 });
 
-// Get all books based on title
+// Get all books based on title (Task 4)
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  var resultSet = [];
+  for(var keys in books){ if(books[keys].title == req.params.title){ resultSet.push(books[keys])} }
+
+  if(resultSet === undefined || resultSet.length == 0){
+    return res.status(404).json({message: "No more results."});
+  }else{
+    return res.status(300).json(resultSet);
+  }
 });
 
-//  Get book review
+//  Get book review (Task 5)
 public_users.get('/review/:isbn',function (req, res) {
+
+  if(books[req.params.isbn].reviews.length == 0 || books[req.params.isbn].reviews.length == null){
+    return res.status(404).json({isbn: req.params.isbn,message: "No more reviews."});
+  }else{
+    return res.status(300).json(books[req.params.isbn].reviews);
+  }
+});
+
+/////////////----------Async-Await functions-------------/////////////////
+
+// Async-Await Get the book list available in the shop (Task 10)
+public_users.get('/async/', async function(req ,res){
+  try{
+    const bookList = await {books};
+    res.status(300).json(bookList);
+  }catch(err){
+    res.status(403).send({message: err});
+  }
+});
+
+// Async-Await Get book details based on ISBN (Task 11)
+public_users.get('/async/isbn/:isbn' ,  async function(req ,res){
+  try{
+    const bookDetails = await books[req.params.isbn];
+    res.status(300).json(bookDetails);
+  }catch(err){
+    res.status(403).send({message: err});
+  }
+});
+
+// Async-Await Get book details based on author (Task 12)
+public_users.get('/async/author/:author',async function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  var resultList = [];
+  try{
+    const bookList = await books;
+    for(var keys in bookList){ 
+      if(bookList[keys].author == req.params.author){
+         resultList.push(bookList[keys])
+        } 
+    }
+    if(resultList === undefined || resultList.length == 0){
+      return res.status(404).json({message: "No more results."});
+    }else{
+      return res.status(300).json(resultList);
+    }
+  }catch(err){
+    res.status(403).send({message: err});
+  }
+});
+
+// Async-Await Get all books based on title (Task 13)
+public_users.get('/title/:title',async function (req, res) {
+  var resultList = [];
+  const bookList = await books;
+  try{
+    for(var keys in bookList){ 
+      if(bookList[keys].title == req.params.title){  resultSet.push(bookList[keys]) } 
+    }
+
+    if(resultList === undefined || resultList.length == 0){
+      return res.status(404).json({message: "No more results."});
+    }else{
+      return res.status(300).json(resultList);
+    }
+  }catch(err){
+    res.status(403).send({message: err});
+  }  
 });
 
 module.exports.general = public_users;
